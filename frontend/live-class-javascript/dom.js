b7e23fbf7572618891ce70f181fabebf593dc0f2
mdmuche <abulamartins@gmail.com>
2026-01-13 13:16:08 +0100

    add: changing attribute using js

--- frontend/live-class-javascript/dom.js
+++ frontend/live-class-javascript/dom.js
@@ -1,3 +1,7 @@
+//? getElementById;
+//? getElementByClassName
+//? querySelector
+//? querySelectorAll
 let heading = document.getElementById('heading');
 
 heading.style.backgroundColor = 'red';
@@ -13,4 +17,21 @@ for(let i = 0; i < para.length; i++){
     }));
 }
 
-paras[0].innerHTML = '<h2>I am the new Heading<h2>'
+paras[0].innerHTML = '<h2>I am the new Heading<h2>';
+
+// para.map((p) => console.log(p)); //! this does not work because getElementByClassName returns a htmlcollection which array mathods will not work on
+
+//? getElementByTagName
+const listItem = document.getElementsByTagName('li');
+// listItem.map(item => console.log(item)); //! this does not work because getElementByClassName returns a htmlcollection which array mathods will not work on
+
+const btn = document.getElementsByClassName('btn')[0];
+const img = document.getElementsByTagName('img')[0];
+// https://placeholder
+
+btn.addEventListener('click', () => {
+    // img.src = './img/card-2.png';
+   img.getAttribute('img');
+   img.setAttribute('src', './img/card-2.png');
+   img.setAttribute('alt', 'card 2');
+})
